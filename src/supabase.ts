04bf3fed1b064529c5a94b3fa9478f9b92180896
francescomereu001-cloud <francesco.mereu001@gmail.com>
2026-04-17 19:05:56 +0200

    create supabase client

---
+++ src/supabase.ts
@@ -0,0 +1,6 @@
+import { createClient } from "@supabase/supabase-js";
+
+export const supabase = createClient(
+  "https://txlpkdkvutibhsufgvlo.supabase.co",
+  "sb_publishable_c-BDahL4GOyT_FQTfsIPeA_fyu9jjEz"
+);
